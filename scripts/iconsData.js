5872dc50b23479fe597e4f851c9175d3559808d8
iconsoup <dmo.studio25@gmail.com>
2025-09-10 00:46:47 +0900

    #3-icon: 하트, 하트 두 개 업로드

--- scripts/iconsData.js
+++ scripts/iconsData.js
@@ -1,52 +1,14 @@
 const iconsData = [
-  {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "마카롱",
-    nameEng: "macaron",
-  },
-  {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "컵케이크",
-    nameEng: "cupcake",
-  },  
-  {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "케이크",
-    nameEng: "cake",
-  },
-    {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "스틱 아이스크림",
-    nameEng: "stick-ice-cream",
-  },
-    {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "콘 아이스크림",
-    nameEng: "cone-ice-cream",
-  },
-    {
-    catKor: "디저트",
-    catEng: "desserts",
-    nameKor: "푸딩",
-    nameEng: "pudding",
-  },
-  {
-    catKor: "사물",
-    catEng: "objects",
-    nameKor: "선물상자",
-    nameEng: "gift-box",
-  },
-  {
-    catKor: "사물",
-    catEng: "objects",
-    nameKor: "확성기",
-    nameEng: "megaphone",
-  },
+  { catKor: "디저트", catEng: "desserts", nameKor: "마카롱", nameEng: "macaron" },
+  { catKor: "디저트", catEng: "desserts", nameKor: "컵케이크", nameEng: "cupcake" },
+  { catKor: "디저트", catEng: "desserts", nameKor: "케이크", nameEng: "cake" },
+  { catKor: "디저트", catEng: "desserts", nameKor: "스틱 아이스크림", nameEng: "stick-ice-cream" },
+  { catKor: "디저트", catEng: "desserts", nameKor: "콘 아이스크림", nameEng: "cone-ice-cream" },
+  { catKor: "디저트", catEng: "desserts", nameKor: "푸딩", nameEng: "pudding" },
+  { catKor: "사물", catEng: "objects", nameKor: "선물상자", nameEng: "gift-box" },
+  { catKor: "사물", catEng: "objects", nameKor: "확성기", nameEng: "megaphone" },
+  { catKor: "기호", catEng: "symbols", nameKor: "하트", nameEng: "heart" },
+  { catKor: "기호", catEng: "symbols", nameKor: "하트 두 개", nameEng: "two-hearts" },
 ];
 
 // 카테고리: emotion, people, animals, nature, food, activities, travel, objects, symbols
